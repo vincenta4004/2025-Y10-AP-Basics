b52686e27fa9a28ca94bc1597bcc035fa9202359
vincenta4004 <vincenta4004@masseyhigh.school.nz>
2025-08-27 10:30:36 +1200

    Variables basics

---
+++ P_02_variables.py
@@ -0,0 +1,14 @@
+# Code to learn about variables...
+from Tools.scripts.fixdiv import multi_ok
+
+# Variables can be a string (note the speech marks)
+greeting = "hello world"
+
+# Variables can be a number
+fav_num = 7
+num_string = "5"
+
+# Constants are uppercase
+PI = 3.14
+
+a_01_my_num = 7
